ecbe548c821edff8e9f70d9f57e376b2f2f9b6e4
dagrooms52 <dagn9c@mst.edu>
2015-06-15 00:57:54 -0500

    Initial Commit

---
+++ Source/constants.py
@@ -0,0 +1,16 @@
+import math
+
+
+sc_height = 900
+sc_width = 500
+
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+BLUE     = (   0,   0, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+GRAY     = ( 127, 127, 127)
+CYAN     = (   0, 255, 255)
+ELECTRICBLUE = (8, 146, 208)
+
+PI = math.pi
